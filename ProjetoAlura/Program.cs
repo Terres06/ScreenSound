11273e9b6b4aebfba151687bf52d83269984ced5
pedro <pedro@TERRES-DESKTOP>
2025-07-07 18:19:37 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoAlura/Program.cs
@@ -0,0 +1,28 @@
+﻿string mensagemDeBoasVindas = "Bem vindo ao screen sound!";
+
+void ExibeBoasVindas() 
+{
+    Console.WriteLine(@"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
+    Console.WriteLine(mensagemDeBoasVindas);
+}
+
+void ExibirOpccoes()
+{
+    Console.WriteLine("Selecione uma opção:");
+    Console.WriteLine("1 - Registrar uma banda");
+    Console.WriteLine("2 - Mostrar todas as bandas ");
+    Console.WriteLine("3 - Avaliar uma banda");
+    Console.WriteLine("4 - Exibir media de uma banda");
+    Console.WriteLine("0 - Sair");
+
+}
+
+ExibeBoasVindas();
+ExibirOpccoes();
